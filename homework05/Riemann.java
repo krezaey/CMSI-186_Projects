6586ecb8745b57d526846aa4aee36e0bb153a236
kckc1289 <keziah.camille8@ymail.com>
2019-03-21 18:06:02 -0700

    Set up Riemann class and outlined methods to create

---
+++ homework05/Riemann.java
@@ -0,0 +1,53 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Riemann.java
+ *  Purpose       :  Main program to run Riemann sum calculator
+ *  Author        :  Keziah Rezaey
+ *  Date Created  :  2019-03-21
+ *  Due Date      :  2019-04-02
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+ public class Riemann {
+
+    public Riemann () {}
+
+    public void handleInitialArguments() {
+        System.out.println("Hello");
+    }
+
+    public void detectString() {
+        System.out.println("Hello");
+    }
+
+    public void setupSum() {
+        System.out.println("Hello");
+    }
+
+    public double rightHandSum() {
+        return 0;
+    }
+
+    public double leftHandSum() {
+        return 0;
+    }
+
+    public double trapezoidalSum() {
+        return 0;
+    }
+
+    public double simpsonSum() {
+        return 0;
+    }
+
+    public double calculateFourthDerivative() {
+        return 0;
+    }
+
+    public double calculateError() {
+        return 0;
+    }
+
+    public void runTests() {
+        System.out.println("Hello");
+    }
+ }
