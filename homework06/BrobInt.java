488ff01cc82c1a7c2bf3f8c49310744aed7a24c0
kckc1289 <keziah.camille8@ymail.com>
2019-04-03 15:00:15 -0700

    Added methods to do

---
+++ homework06/BrobInt.java
@@ -0,0 +1,55 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  BrobInt.java
+ *  Purpose       :  Main program to run BrobInt class
+ *  Author        :  Keziah Rezaey
+ *  Date Created  :  2019-04-03
+ *  Due Date      :  2019-04-24
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+ public class BrobdingnagianInt {
+
+    public static final BrobInt ZERO = 0;
+    public static final BrobInt ONE = 1;
+    public static final BrobInt TEN = 10;
+
+    //constructor
+    public BrobInt ( String value) {}
+
+    public BrobInt add ( BrobInt value ) {
+
+    }
+
+    public BrobInt subtract ( BrobInt value ) {
+
+    }
+
+    public BrobInt multiply ( BrobInt value ) {
+
+    }
+
+    public BrobInt divide ( BrobInt value ) {
+
+    }
+
+    public BrobInt remainder ( BrobInt value ) {
+
+    }
+
+    public String toString() {
+
+    }
+
+    public int compareTo ( BrobInt value ) {
+
+    }
+
+    public boolean equals ( Object x ) {
+
+    }
+
+    public static BrobInt valueOf ( long value ) {
+
+    }
+
+ }
