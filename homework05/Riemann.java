4ba184e652f7651b1a12099fa6ca998c7363727e
kckc1289 <keziah.camille8@ymail.com>
2019-04-04 01:40:38 -0700

    Adjusted special cases

--- homework05/Riemann.java
+++ homework05/Riemann.java
@@ -9,21 +9,20 @@
 
  /**
   * Need to fix: percent arguments with e not working
-  * java Riemann poly 2 1 2 -1 need to throw nfe
-  * java Riemann poly 0 1 2 0 need to throw nfe
   * java Riemann poly 1.0 -2.1 3.2 -10.0 5.0 throwing nfe, shouldn't
   * java Riemann poly -3.0 -7.0 -1.0 -2.0 0.0 1.0 0.0 2.0 1e-6% throwing nfe, shouldn't
   * java Riemann sin -17.0 3.0 -11.0 11.0 1e-7% taking a long time to integrate
-  * java Riemann sin -17.0 1.0 -23.0 23.0 1e-4% throwing nfe, shouldn't
+  * java Riemann sin -17.0 1.0 -23.0 23.0 1e-4% taking a long time to integrate
   * java Riemann sqrt 23.0 -3.0 7.0 1.0 7.0 1e-6% very off!
   * java Riemann sqrt 100.0 0.0 1.0 1e-8% throwing nfe, shouldn't
   * java Riemann sqrt 2.0 0.0 1.0 1e-3%, throwing nfe, shouldn't
-  *
   */
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 
  public class Riemann {
 
@@ -178,7 +177,8 @@ import java.util.List;
 
         if ( functionValid == false || boundsValid == false ) {
             switch (functionArgument) {
-                case "poly": if ( args.length < 4 ) {throw new NumberFormatException(exceptionMessage);} break;
+                case "poly": if ( ((args.length <= 4) && (args[args.length - 1].contains("%") == false)) 
+                                || ( ((Double.parseDouble(args[args.length - 1])) <= 0) && (args[args.length - 1].contains("%") == false)) ) {throw new NumberFormatException(exceptionMessage);} break;
                 default: if ( args.length < 3 ) {throw new NumberFormatException(exceptionMessage);} break;
             }
         } else if (args.length < 1) {
@@ -204,6 +204,7 @@ import java.util.List;
      * Method that integrates the functions based on solved y values
      * @param String args[] from terminal, double q value
      * @return double value calculated from integrated area
+     * @throws nfe when appropriate (for domain)
      */
 
     public double integrate( String[] args, double q ) {
@@ -212,27 +213,27 @@ import java.util.List;
 
         switch (functionArgument) {
             case "poly": 
-                for ( double j = lowerBound; j < upperBound; j += dx ) {
+                for ( double j = lowerBound + (dx / 2); j < upperBound; j += dx ) {
                     area += ( getPolyValue(j) * dx );
                 }   
                 break;
             case "sin": 
-                for ( double j = lowerBound; j < upperBound; j += dx ) {
+                for ( double j = lowerBound + (dx / 2); j < upperBound; j += dx ) {
                     area += ( Math.sin(j) * dx );
                 }
                 break;
             case "cos": 
-                for ( double j = lowerBound; j < upperBound; j += dx ) {
+                for ( double j = lowerBound + (dx / 2); j < upperBound; j += dx ) {
                     area += ( Math.cos(j) * dx );
                 }
                 break;
             case "tan": 
-                for ( double j = lowerBound; j < upperBound; j += dx ) {
+                for ( double j = lowerBound + (dx / 2); j < upperBound; j += dx ) {
                     area += ( Math.tan(j) * dx );
                 }
                 break;
             case "sqrt": 
-                for ( double j = lowerBound; j < upperBound; j += dx ) {
+                for ( double j = lowerBound + (dx / 2); j < upperBound; j += dx ) {
                     if (j > 0) {
                         area += ( Math.sqrt(j) * dx );
                     }
@@ -242,12 +243,12 @@ import java.util.List;
                 }  
                 break;
             case "log": 
-                for ( double j = lowerBound; j < upperBound; j += dx ) {
+                for ( double j = lowerBound + (dx / 2); j < upperBound; j += dx ) {
                     area += ( Math.log(j) * dx );
                 }
                 break;
             case "exp": 
-                for ( double j = lowerBound; j < upperBound; j += dx ) {
+                for ( double j = lowerBound + (dx / 2); j < upperBound; j += dx ) {
                     area += ( Math.exp(j) * dx );
                 }
                 break;
@@ -312,11 +313,14 @@ import java.util.List;
      * Main program runs here! Uses methods from Riemann class.
      */
     public static void main ( String args[] ) {
+
         Riemann r = new Riemann();
+
+        DecimalFormat integralRound = new DecimalFormat( ".0000" );
 
         r.handleInitialArguments(args);
 
-        System.out.println( "\n\n  Welcome to the Riemann Integral calculator! We calculate left-hand sums for you!" );
+        System.out.println( "\n\n  Welcome to the Riemann Integral calculator!" );
         System.out.println( "  We are integrating the function " + r.functionArgument + " from " + r.lowerBound + " to " + r.upperBound + " with an accuracy of " + r.percentArgument + "%."  );
         System.out.println( "  **************************************************************************************\n\n" );
 
@@ -330,11 +334,11 @@ import java.util.List;
             double current = r.integrate(args, q);
             
             if ( ( Math.abs(1 - (previous/current))  <= ( r.percentArgument / 100.0 ) ) ) {
-                System.out.println("  The integral value is: " + previous + ".\n\n");
+                String result = String.valueOf( integralRound.format(previous) );
+                System.out.println("  The integral value is: " + result + ".\n\n");
                 System.exit(0);
                 break;     
             }
-        
             previous = current;
             q++;
         }
@@ -353,5 +357,3 @@ import java.util.List;
         // result = validatArgs(myArgs);
         // System.out.println("Expected true, got " + result);
 //  }
-// 
-// check to see if asking for sqrt of negative number
